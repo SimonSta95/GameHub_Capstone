9b9eea627a08b44d940adad53d3455c04ad01f97
Simon Staß <simonsta95@gmail.com>
2024-08-26 14:51:21 +0200

    added some styling to the footer

--- gamehub-frontend/src/App.tsx
+++ gamehub-frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
 
   return (
-      <h1>App</h1>
+      <>
+        <h1>App</h1>
+        <Footer/>
+      </>
   )
 }
 
--- gamehub-frontend/src/components/Footer/Footer.tsx
+++ gamehub-frontend/src/components/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import "./Footer.css"
+import {Box, Container, Typography} from "@mui/material";
 
 export default function Footer() {
     return (
-        <>
-
-        </>
+        <Box
+            component="footer"
+            sx={{
+                bgcolor: 'primary.main',
+                color: 'primary.contrastText',
+                py: 3,
+                mt: 'auto',
+                textAlign: 'center',
+            }}
+        >
+            <Container className={"footer-container"} maxWidth="lg">
+                <img className={"footer-logo"} src={"src/assets/gamehub-white.png"}  alt={"logo"}/>
+                <Typography variant="caption text">
+                    © {new Date().getFullYear()} Simon Staß. All rights reserved.
+                </Typography>
+            </Container>
+        </Box>
     );
 }

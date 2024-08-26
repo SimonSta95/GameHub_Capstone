5d5663af1c9559217c6943e877530e82a9b9d683
Simon Staß <simonsta95@gmail.com>
2024-08-26 14:27:33 +0200

    Add theme.ts, Switched from Chakra UI to Material UI

--- gamehub-frontend/src/main.tsx
+++ gamehub-frontend/src/main.tsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {ChakraProvider} from "@chakra-ui/react";
 import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "@mui/material";
+import theme from "./theme/theme.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
-          <ChakraProvider>
+          <ThemeProvider theme={theme}>
             <App />
-          </ChakraProvider>
+          </ThemeProvider>
       </BrowserRouter>
   </StrictMode>,
 )

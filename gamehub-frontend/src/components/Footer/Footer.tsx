4867b6134d7f6880ca30ee872460013a71ef338a
Simon Staß <simonsta95@gmail.com>
2024-08-26 17:08:42 +0200

    Add footer

--- gamehub-frontend/src/components/Footer/Footer.tsx
+++ gamehub-frontend/src/components/Footer/Footer.tsx
@@ -1,27 +1,14 @@
 import "./Footer.css"
-import {Box, Container, Typography} from "@mui/material";
-import logo from '../../assets/gamehub-white.png';
+import {Box, Typography} from "@mui/material";
+import logo from "../../assets/gamehub-white.png";
 
 export default function Footer() {
     return (
-        <Box
-            component="footer"
-            sx={{
-                bgcolor: 'primary.main',
-                color: 'primary.contrastText',
-                py: 3,
-                mt: 'auto',
-                textAlign: 'center',
-            }}
-        >
-            <Container className={"footer-container"}
-                       maxWidth="lg"
-            >
-                <img className={"footer-logo"} src={logo} alt={"logo"}/>
-                <Typography variant="caption">
-                    © {new Date().getFullYear()} Simon Staß. All rights reserved.
-                </Typography>
-            </Container>
+        <Box component={"footer"} className={"footer"}>
+            <img className={"footer-logo"} src={logo} alt={"logo"}/>
+            <Typography variant={"caption"} className={"footer-text"}>
+                © {new Date().getFullYear()} Simon Staß. All rights reserved.
+            </Typography>
         </Box>
     );
 }

d1b669010f4a7d1718fdc90eaec28fc34509d278
Simon Staß <simonsta95@gmail.com>
2024-08-27 09:57:48 +0200

    Add Exception handling for User not found

--- gamehub-backend/src/main/java/com/example/gamehubbackend/exceptions/GlobalExceptionHandler.java
+++ gamehub-backend/src/main/java/com/example/gamehubbackend/exceptions/GlobalExceptionHandler.java
@@ -9,9 +9,20 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(GameNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public CustomErrorMessage handleGameNotFoundException(GameNotFoundException e) {
+        return new CustomErrorMessage(
+                e.getMessage(),
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value()
+        );
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public CustomErrorMessage handleUserNotFoundException(UserNotFoundException e) {
         return new CustomErrorMessage(
                 e.getMessage(),
                 LocalDateTime.now(),

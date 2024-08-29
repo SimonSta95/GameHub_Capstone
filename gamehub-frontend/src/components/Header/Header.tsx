6fd0b7181bc8a50678027e0eb0030df8b3bfa18b
Simon Staß <simonsta95@gmail.com>
2024-08-29 11:31:02 +0200

    Added a Header navigation - fixed a lot of css

---
+++ gamehub-frontend/src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import { Avatar } from '@mui/material';
+import logo from '../../assets/gamehub-color.png';
+import './Header.css';
+import {User} from "../../types.ts";
+
+type HeaderProps = {
+    user?: User | null;
+    onLogin: () => void;
+    onLogout: () => void;
+};
+
+export default function Header(props: Readonly<HeaderProps>) {
+
+    return (
+        <header className="header">
+            <Link to="/" className="logo-link">
+                <img className="logo" src={logo} alt="GameHub Logo"/>
+            </Link>
+            {props.user && <Link to="/games" className="nav-link">Games</Link>}
+            <nav className="nav">
+                {props.user ? (
+                    <div className="user-info">
+                        <Avatar className="avatar" alt={props.user.username} src={props.user.avatarUrl}/>
+                        <span className="username">{props.user.username}</span>
+                        <button className="auth-button" onClick={props.onLogout}>Logout</button>
+                    </div>
+                ) : (
+                    <button className="auth-button" onClick={props.onLogin}>GitHub Login</button>
+                )}
+            </nav>
+        </header>
+    )
+}

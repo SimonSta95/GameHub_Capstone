b62946b0d1b0eb0fb7939cf7480558fddecb5b40
Simon Staß <simonsta95@gmail.com>
2024-09-02 15:13:12 +0200

    add controller and service functionality

--- gamehub-backend/src/main/java/com/example/gamehubbackend/controllers/NoteController.java
+++ gamehub-backend/src/main/java/com/example/gamehubbackend/controllers/NoteController.java
@@ -1,9 +1,42 @@
 package com.example.gamehubbackend.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.gamehubbackend.models.Note;
+import com.example.gamehubbackend.models.NoteDTO;
+import com.example.gamehubbackend.services.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notes")
+@RequiredArgsConstructor
 public class NoteController {
+
+    private final NoteService noteService;
+
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteService.getAllNotes();
+    }
+
+    @GetMapping("/{id}")
+    public Note getNoteById(@PathVariable String id) {
+        return noteService.getNoteById(id);
+    }
+
+    @PostMapping
+    public Note createNote(@RequestBody NoteDTO noteDTO) {
+        return noteService.createNote(noteDTO);
+    }
+
+    @PutMapping(path = {"{id}/update", "{id}"})
+    public Note updateNote(@PathVariable String id ,@RequestBody NoteDTO noteDTO) {
+        return noteService.updateNote(id,noteDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable String id) {
+        noteService.deleteNote(id);
+    }
 }

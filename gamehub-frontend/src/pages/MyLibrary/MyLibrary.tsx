aad883cbfd8ca891c3fc941ac20674c423006c65
Simon Staß <simonsta95@gmail.com>
2024-09-02 12:59:43 +0200

    minor layout fix

--- gamehub-frontend/src/pages/MyLibrary/MyLibrary.tsx
+++ gamehub-frontend/src/pages/MyLibrary/MyLibrary.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import GameCard from "../../components/GameCard/GameCard.tsx";
 import { Game, User } from "../../types.ts";
 

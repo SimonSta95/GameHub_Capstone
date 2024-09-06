5182bca88411bcc79b7e94358622f41bd922c5d2
Simon Staß <simonsta95@gmail.com>
2024-09-06 12:18:20 +0200

    add needed types

--- gamehub-frontend/src/types.ts
+++ gamehub-frontend/src/types.ts
@@ -19,6 +19,51 @@ export type Game = {
     coverImage: string
 }
 
+export type GameAPIResponse = {
+    next: string,
+    pervious: string,
+    games: GameAPI[],
+}
+
+export type GameAPI = {
+    id: string,
+    title: string,
+    genre: string[],
+    releaseDate: string,
+    platforms: string[],
+    coverImage: string
+}
+
+export type GameDetailAPIResponse = {
+    id: number;
+    name: string;
+    description: string;
+    released: string;
+    background_image: string;
+    platforms: platform[];
+    developers: Developer[];
+    genres: Genre[];
+    publishers: Publisher[];
+}
+
+export type platform = {
+    platform: {
+        name: string
+    }
+}
+
+export type Developer = {
+    name: string;
+};
+
+export type Genre = {
+    name: string;
+};
+
+export type Publisher = {
+    name: string;
+};
+
 export type Note = {
     id: string;
     userId: string;

976a1aa8cc96e8d06c4a29168c91e91a9fe8cdb5
Simon Staß <simonsta95@gmail.com>
2024-09-13 11:52:08 +0200

    Test fix

--- gamehub-backend/src/test/java/com/example/gamehubbackend/services/NoteServiceUnitTests.java
+++ gamehub-backend/src/test/java/com/example/gamehubbackend/services/NoteServiceUnitTests.java
@@ -24,8 +24,8 @@ class NoteServiceUnitTests {
     @Test
     void getAllNotes_Test() {
         List<Note> notes = List.of(
-                new Note("1", "user1", "game1", "Title 1", "Content 1", "category1", localDateTime, updateDateTime),
-                new Note("2", "user2", "game2", "Title 2", "Content 2", "category2", localDateTime, updateDateTime)
+                new Note("1", "Test","user1", "game1", "Title 1", "Content 1", "category1", localDateTime, updateDateTime),
+                new Note("2", "Test","user2", "game2", "Title 2", "Content 2", "category2", localDateTime, updateDateTime)
         );
 
         when(noteRepository.findAll()).thenReturn(notes);
@@ -37,7 +37,7 @@ class NoteServiceUnitTests {
 
     @Test
     void getNoteById_Test() {
-        Note note = new Note("1", "user1", "game1", "Title 1", "Content 1", "category1", localDateTime, updateDateTime);
+        Note note = new Note("1", "Test","user1", "game1", "Title 1", "Content 1", "category1", localDateTime, updateDateTime);
         when(noteRepository.findById("1")).thenReturn(Optional.of(note));
 
         Note actualNote = noteService.getNoteById("1");
@@ -55,8 +55,8 @@ class NoteServiceUnitTests {
 
     @Test
     void createNote_Test() {
-        NoteDTO noteDTO = new NoteDTO("user1", "game1", "Title 1", "Content 1", "category1", localDateTime, updateDateTime);
-        Note noteToSave = new Note("1", "user1", "game1", "Title 1", "Content 1", "category1", LocalDateTime.now(), LocalDateTime.now());
+        NoteDTO noteDTO = new NoteDTO("user1", "Test","game1", "Title 1", "Content 1", "category1", localDateTime, updateDateTime);
+        Note noteToSave = new Note("1", "Test","user1", "game1", "Title 1", "Content 1", "category1", LocalDateTime.now(), LocalDateTime.now());
 
         when(idService.randomId()).thenReturn("1");
         when(noteRepository.save(any(Note.class))).thenReturn(noteToSave);
@@ -71,9 +71,9 @@ class NoteServiceUnitTests {
     @Test
     void updateNote_Test() {
         String noteId = "1";
-        Note existingNote = new Note("1", "user1", "game1", "Old Title", "Old Content", "category1", localDateTime, updateDateTime);
-        NoteDTO updatedNoteDTO = new NoteDTO("user1", "game1", "Updated Title", "Updated Content", "category1", localDateTime, updateDateTime);
-        Note updatedNote = new Note("1", "user1", "game1", "Updated Title", "Updated Content", "category1", localDateTime, LocalDateTime.now());
+        Note existingNote = new Note("1", "Test","user1", "game1", "Old Title", "Old Content", "category1", localDateTime, updateDateTime);
+        NoteDTO updatedNoteDTO = new NoteDTO("user1", "Test","game1", "Updated Title", "Updated Content", "category1", localDateTime, updateDateTime);
+        Note updatedNote = new Note("1", "Test","user1", "game1", "Updated Title", "Updated Content", "category1", localDateTime, LocalDateTime.now());
 
         when(noteRepository.findById(noteId)).thenReturn(Optional.of(existingNote));
         when(noteRepository.save(any(Note.class))).thenReturn(updatedNote);
@@ -88,7 +88,7 @@ class NoteServiceUnitTests {
     @Test
     void updateNote_NoteNotFound_Test() {
         String noteId = "1";
-        NoteDTO updatedNoteDTO = new NoteDTO("user1", "game1", "Updated Title", "Updated Content", "category1", localDateTime, updateDateTime);
+        NoteDTO updatedNoteDTO = new NoteDTO("user1", "Test","game1", "Updated Title", "Updated Content", "category1", localDateTime, updateDateTime);
 
         when(noteRepository.findById(noteId)).thenReturn(Optional.empty());
 

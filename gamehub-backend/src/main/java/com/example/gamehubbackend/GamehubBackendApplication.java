0ae62136d4e795f1f71a4fd31ef02a4ceaa67244
Simon Staß <simonsta95@gmail.com>
2024-09-16 12:17:28 +0200

    Added Comments in all relevant files, added some small changes like typing etc.

--- gamehub-backend/src/main/java/com/example/gamehubbackend/GamehubBackendApplication.java
+++ gamehub-backend/src/main/java/com/example/gamehubbackend/GamehubBackendApplication.java
@@ -8,17 +8,17 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableCaching
+@EnableCaching // Enables support for caching in the application
 public class GamehubBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamehubBackendApplication.class, args);
 	}
 
+	// Bean definition for the password encoder
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
 	}
-
 
 }
